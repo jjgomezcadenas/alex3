9c0d1f0fa86cfdbf3d774348cd1f8082af6a84ba
jmunozv <javier.munoz.vidal@gmail.com>
2015-02-21 16:44:28 +0100

    Adding missed jmalgos Pfiles

---
+++ jmalgos/PDrawHits.h
@@ -0,0 +1,14 @@
+
+#ifndef PDrawHits_
+#define PDrawHits_
+
+namespace alex {
+  class PDrawHits {
+  public:
+    // You can add here public functions and variables to be used by derived algos
+  protected:
+    // You can add here protected functions and variables to be used by derived algos
+    
+  };
+}
+#endif 
---
+++ jmalgos/PTestAlex.h
@@ -0,0 +1,16 @@
+
+#ifndef PTestAlex_
+#define PTestAlex_
+
+namespace alex {
+  class PTestAlex {
+
+  public:
+    // You can add here public functions and variables to be used by derived algos
+
+  protected:
+    // You can add here public functions and variables to be used by derived algos
+
+  };
+}
+#endif 
