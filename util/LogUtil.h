1c4e55804d1f29dde2917976dcc4fecafd551ef5
jjgomezcadenas <jjgomezcadenas@gmail.com>
2015-02-21 10:26:14 +0100

    fixing Util

--- util/LogUtil.h
+++ util/LogUtil.h
@@ -7,13 +7,13 @@
 */
 
 
-#include "log4cpp/Category.hh"
-#include "log4cpp/Appender.hh"
-#include "log4cpp/FileAppender.hh"
-#include "log4cpp/OstreamAppender.hh"
-#include "log4cpp/Layout.hh"
-#include "log4cpp/BasicLayout.hh"
-#include "log4cpp/Priority.hh"
+#include <log4cpp/Category.hh>
+#include <log4cpp/Appender.hh>
+#include <log4cpp/FileAppender.hh>
+#include <log4cpp/OstreamAppender.hh>
+#include <log4cpp/Layout.hh>
+#include <log4cpp/BasicLayout.hh>
+#include <log4cpp/Priority.hh>
 #include <string>
 
 namespace alex {
