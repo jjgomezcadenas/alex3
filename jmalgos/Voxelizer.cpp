f67c0dc5dbd67cdf13c3716a541a7bdf050351a8
jmunozv <javier.munoz.vidal@gmail.com>
2015-02-25 00:07:41 +0100

    Added the check of Emin to voxelizer

--- jmalgos/Voxelizer.cpp
+++ jmalgos/Voxelizer.cpp
@@ -75,56 +75,66 @@ namespace alex {
     }
 
     // Convertion from Paolina Tracks to ARTracks
+    int numInvTrks = 0;
     for (int t=0; t<tracks.size(); t++) {
       ARTrack* arTrk = new ARTrack(t);
       paolina::Track* pTrk = tracks[t];
 
-      //std::cout << "True Energy from Paolina Track: " << pTrk->GetEDep() << std::endl;
-
-      // Converting from Paolina Voxels to AHits
-      for (int v=0; v<pTrk->NVoxels(); v++) {
-        AHit* ahit = new AHit();
-        const paolina::Voxel* pVxl = pTrk->GetVoxel(v);
-        ahit->SetID(v);
-        TVector3 pos;
-        pos.SetXYZ(pVxl->GetPosition().x(),
-                   pVxl->GetPosition().y(),
-                   pVxl->GetPosition().z());
-        ahit->SetPosition(pos);
-        ahit->SetEdep(pVxl->GetEDep());
-        arTrk->AddHit(ahit);
+      // Discarding Invisible PTracks
+      if (pTrk->GetEDep() < fEmin) {
+        numInvTrks += 1;
+        klog << log4cpp::Priority::DEBUG << "Voxelizer::Invisible Track of "
+             << pTrk->GetEDep() << " MeV";
       }
+      // Processing Visible PTracks
+      else {
+        // Converting from Paolina Voxels to AHits
+        for (int v=0; v<pTrk->NVoxels(); v++) {
+          AHit* ahit = new AHit();
+          const paolina::Voxel* pVxl = pTrk->GetVoxel(v);
+          ahit->SetID(v);
+          TVector3 pos;
+          pos.SetXYZ(pVxl->GetPosition().x(),
+                     pVxl->GetPosition().y(),
+                     pVxl->GetPosition().z());
+          ahit->SetPosition(pos);
+          ahit->SetEdep(pVxl->GetEDep());
+          arTrk->AddHit(ahit);
+        }
 
-      // As origin True Tracks are unknown, set to -1
-      arTrk->AddTTrackID(-1);
+        // As origin True Tracks are unknown, set to -1
+        arTrk->AddTTrackID(-1);
 
-      // Setting Spatial Energy
-      arTrk->SetSpatialRes(TVector3(fVoxelX, fVoxelY, fVoxelZ));
+        // Setting Spatial Energy
+        arTrk->SetSpatialRes(TVector3(fVoxelX, fVoxelY, fVoxelZ));
 
-      // Setting Extremes
-      std::pair <int, int> pExtremes = pTrk->GetExtremes();
-      AHit* hit1 = arTrk->GetHit(pExtremes.first);
-      AHit* hit2 = arTrk->GetHit(pExtremes.second);
-      arTrk->SetExtreme1(new AHit(*hit1));
-      arTrk->SetExtreme2(new AHit(*hit2));
+        // Setting Extremes
+        std::pair <int, int> pExtremes = pTrk->GetExtremes();
+        AHit* hit1 = arTrk->GetHit(pExtremes.first);
+        AHit* hit2 = arTrk->GetHit(pExtremes.second);
+        arTrk->SetExtreme1(new AHit(*hit1));
+        arTrk->SetExtreme2(new AHit(*hit2));
 
-      // Adding Ordered hits
-      for (int v=0; v<pTrk->NMainPathVoxels(); v++) {
-        const paolina::Voxel* pVxl = pTrk->GetMainPathVoxel(v);
-        for (auto hit: arTrk->GetHits()) {
-          if ( (hit->GetEdep() == pVxl->GetEDep()) and
-               (hit->GetPosition().x() == pVxl->GetPosition().x()) and
-               (hit->GetPosition().y() == pVxl->GetPosition().y()) and
-               (hit->GetPosition().z() == pVxl->GetPosition().z()) ) {
-            arTrk->AddOrdHit(hit);
-            break;
-          }
-        } 
-      }
+        // Adding Ordered hits
+        for (int v=0; v<pTrk->NMainPathVoxels(); v++) {
+          const paolina::Voxel* pVxl = pTrk->GetMainPathVoxel(v);
+          for (auto hit: arTrk->GetHits()) {
+            if ( (hit->GetEdep() == pVxl->GetEDep()) and
+                 (hit->GetPosition().x() == pVxl->GetPosition().x()) and
+                 (hit->GetPosition().y() == pVxl->GetPosition().y()) and
+                 (hit->GetPosition().z() == pVxl->GetPosition().z()) ) {
+              arTrk->AddOrdHit(hit);
+              break;
+            }
+          } 
+        }
 
-      //arTrk->DisplayInfo(std::cout);
-      ASvc::Instance().AddRTrack(arTrk);
+        //arTrk->DisplayInfo(std::cout);
+        ASvc::Instance().AddRTrack(arTrk);
+      }      
     }
+
+    if (numInvTrks>0) fVoxelizer_NumInvTracks_H1->Fill(numInvTrks);
 
     return true;
   }
