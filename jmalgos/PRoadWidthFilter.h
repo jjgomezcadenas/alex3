d3b1735dbe6152174c9eaded633ad2f158b95475
jmunozv <javier.munoz.vidal@gmail.com>
2015-03-02 14:15:15 +0100

    Rename RoadFilter to RoadWidthFilter

---
+++ jmalgos/PRoadWidthFilter.h
@@ -0,0 +1,18 @@
+
+#ifndef PRoadWidthFilter_
+#define PRoadWidthFilter_
+
+namespace alex {
+  class PRoadWidthFilter {
+
+  public:
+    // You can add here public functions and variables to be used by derived algos
+    
+  protected:
+    // Counters
+	  int fNumInputEvents;    
+	  int fNumOutputEvents;
+    
+  };
+}
+#endif 
